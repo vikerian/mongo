6c5605371e0d4fbe3da291e51727575962fc223d
vilem_kebrt <vilem.kebrt@dcuk.cz>
2025-01-12 12:25:39 +0100

    json->bson

--- mongodoc.go
+++ mongodoc.go
@@ -6,19 +6,19 @@ import (
 )
 
 type MongoDoc struct {
-	Id         uuid.UUID          `json:"uuid"`
-	MongoID    primitive.ObjectID `json:"mongo_id,omitempty"`
-	Collection string             `json:"mongo_collection,omitempty"`
-	Name       string             `json:"doc_name"`
-	Version    string             `json:"doc_version,omitempty"`
-	Authors    []Author           `json:"authors,omitempty"`
-	Raw        []byte             `json:"raw,omitempty"`
-	Document   interface{}        `json:"doc_structured,omitempty"`
-	Data       []interface{}      `json:"data,omitempty"`
+	Id         uuid.UUID          `bson:"uuid"`
+	MongoID    primitive.ObjectID `bson:"mongo_id,omitempty"`
+	Collection string             `bson:"mongo_collection,omitempty"`
+	Name       string             `bson:"doc_name"`
+	Version    string             `bson:"doc_version,omitempty"`
+	Authors    []Author           `bson:"authors,omitempty"`
+	Raw        []byte             `bson:"raw,omitempty"`
+	Document   interface{}        `bson:"doc_structured,omitempty"`
+	Data       []interface{}      `bson:"data,omitempty"`
 }
 
 type Author struct {
-	Id      uuid.UUID `json:"author_id,omitempty"`
-	Name    string    `json:"author_name,omitempty"`
-	Contact []string  `json:"author_contact_informations,omitempty"`
+	Id      uuid.UUID `bson:"author_id,omitempty"`
+	Name    string    `bson:"author_name,omitempty"`
+	Contact []string  `bson:"author_contact_informations,omitempty"`
 }
